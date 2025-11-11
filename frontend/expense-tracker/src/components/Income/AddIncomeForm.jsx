3a65a11480b91830c2facfc5786b433f4c63c119
jyoti <jyotidhondirampatil@gmail.com>
2025-11-11 11:30:05 +0530

    Error Resolved

--- frontend/expense-tracker/src/components/Income/AddIncomeForm.jsx
+++ frontend/expense-tracker/src/components/Income/AddIncomeForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Input from '../inputs/Input'
+import Input from '../Inputs/Input'
 import EmojiPickerPopUp from '../EmojiPickerPopUp'
 
 const AddIncomeForm = ({ onAddIncome }) => {
